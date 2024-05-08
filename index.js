91dc2393db2f0685f0e0060d5a4751ae1f8b6654
Markeidus Hall II <mhallii@umich.edu>
2024-05-08 00:44:37 -0400

    adds some cors stuff

--- index.js
+++ index.js
@@ -21,8 +21,14 @@ const startDB = async () => {
   }
 }
 
-startDB()
+startDB();
 
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.static(path));
 app.use(cors());
 app.use(morgan('tiny'));
