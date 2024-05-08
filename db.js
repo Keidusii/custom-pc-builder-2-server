6ebcbbbd33ce5c4d2ab846f84eaac71277abac34
Markeidus Hall II <mhallii@umich.edu>
2024-05-07 21:59:45 -0400

    add password field to client

--- db.js
+++ db.js
@@ -4,6 +4,7 @@ const pool = new Client({
   user: process.env.DATA_USER,
   host: process.env.HOST,
   port: process.env.DATA_PORT,
+  password: process.env.DATA_PASS,
   database: process.env.DATABASE_NAME
 });
 
